388d3b7b4459a202e4f5ec4ddf24ba4019283e5d
isaakAtChess <157117664+isaakAtChess@users.noreply.github.com>
2024-01-23 10:37:13 +0000

    i dont know

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 // node script.js
+ const side = prompt("Enter the side to play: ")
+ if (side="white"){
+    let cars = [[bR,bN,bB,bQ,Bk,bB,bN,bR],
+    [bP,bP,bP,bP,bP,bP,bP,bP],
+    [E,E,E,E,E,E,E,E],
+    [E,E,E,E,E,E,E,E],
+    [E,E,E,E,E,E,E,E],
+    [E,E,E,E,E,E,E,E],
+]
+ }
+
+
+
+
+
